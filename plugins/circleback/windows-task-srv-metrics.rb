fd8156b0b28df0eb6a8ca77410a7685b68f2c6e3
Kyle Burckhard <kyle.burckhard@circleback.com>
2014-10-07 16:08:10 -0700

    Clean up syntax

--- plugins/circleback/windows-task-srv-metrics.rb
+++ plugins/circleback/windows-task-srv-metrics.rb
@@ -22,14 +22,14 @@ class TypePerfMetric < Sensu::Plugin::Metric::CLI::Graphite
     :default => "#{Socket.gethostname}"
 
   def get_matrics_hash(metrics)
-	metric = metrics.values.map {|m| %Q("#{m}")}.join(' ')
+    metric = metrics.values.map {|m| %Q("#{m}")}.join(' ')
     tempArr   = []
 
     command   = %Q(typeperf -sc 1 #{metric})
     IO.popen(command) { |io| io.each { |line| tempArr.push(line) } }
 
-	names = tempArr[1].split(",")[1..-1].map {|n| metrics.invert[n.match(/(\\Process.+)/)[1][0..-2]] }
-    stats = tempArr[2].split(",")[1..-1].map {|s| sprintf "%.2f",s[1..-3].to_f	}
+    names = (tempArr[1].split(",")[1..-1] || []).map {|n| metrics.invert[n.match(/(\\Process.+)/)[1][0..-2]] }
+    stats = (tempArr[2].split(",")[1..-1] || []).map {|s| sprintf "%.2f", s[1..-3].to_f	}
     Hash[names.zip(stats)]
   end
 
@@ -39,42 +39,42 @@ class TypePerfMetric < Sensu::Plugin::Metric::CLI::Graphite
     command   = %Q(typeperf -sc 1 "\\Process(#{matcher})\\ID Process")
     IO.popen(command) { |io| io.each { |line| tempArr.push(line) } }
 
-    tempArr[1].split(",")[1..-1].map{ |s| s.match(/Process\((.+)\)/)[1] }
+    (tempArr[1] || "").split(",")[1..-1].map{ |s| s.match(/Process\((.+)\)/)[1] }
   end
 
-  def flat_hash(h,f=[],g={})
-	return g.update({ f=>h }) unless h.is_a? Hash
-	h.each { |k,r| flat_hash(r,f+[k],g) }
-	g
+  def flat_hash(h, f = [], g = {})
+    return g.update({ f=>h }) unless h.is_a? Hash
+    h.each { |k, r| flat_hash(r, f+[k], g) }
+    g
   end
 
   def run
     timestamp = Time.now.utc.to_i
-	processes = get_processes("DubLabs.*.Task*")
+    processes = get_processes("DubLabs.*.Task*")
 
     metrics = {
-		"task_srv_count" => processes.size
+      "task_srv_count" => processes.size
     }
 
-	processes.each_with_index do |name, idx|
-		metrics["task_srv_#{idx}"] = get_matrics_hash({
-			pid:			"\\Process(#{name})\\ID Process",
-			proc_prc: 		"\\Process(#{name})\\% Processor Time",
-			user_prc: 		"\\Process(#{name})\\% User Time",
-			priv_prc: 		"\\Process(#{name})\\% Privileged Time",
-			pg_faults_ps: 	"\\Process(#{name})\\Page Faults/sec",
-			thrd_cnt: 		"\\Process(#{name})\\Thread Count",
-			uptime: 		"\\Process(#{name})\\Elapsed Time",
-			handles: 		"\\Process(#{name})\\Handle Count",
-			io_read_bps: 	"\\Process(#{name})\\IO Read Bytes/sec",
-			io_write_bps: 	"\\Process(#{name})\\IO Write Bytes/sec",
-			io_data_bps: 	"\\Process(#{name})\\IO Data Bytes/sec",
-			io_other_bps: 	"\\Process(#{name})\\IO Other Bytes/sec"
-		})
-	end
+    processes.each_with_index do |name, idx|
+      metrics["task_srv_#{idx}"] = get_matrics_hash({
+        pid:          "\\Process(#{name})\\ID Process",
+        proc_prc:     "\\Process(#{name})\\% Processor Time",
+        user_prc:     "\\Process(#{name})\\% User Time",
+        priv_prc:     "\\Process(#{name})\\% Privileged Time",
+        pg_faults_ps: "\\Process(#{name})\\Page Faults/sec",
+        thrd_cnt:     "\\Process(#{name})\\Thread Count",
+        uptime:       "\\Process(#{name})\\Elapsed Time",
+        handles:      "\\Process(#{name})\\Handle Count",
+        io_read_bps:  "\\Process(#{name})\\IO Read Bytes/sec",
+        io_write_bps: "\\Process(#{name})\\IO Write Bytes/sec",
+        io_data_bps:  "\\Process(#{name})\\IO Data Bytes/sec",
+        io_other_bps: "\\Process(#{name})\\IO Other Bytes/sec"
+      })
+    end
 
     flat_hash(metrics).each do |keys, value|
-		output [config[:scheme], keys].flatten.join("."), value, timestamp
+      output [config[:scheme], keys].flatten.join("."), value, timestamp
     end
     ok
   end
